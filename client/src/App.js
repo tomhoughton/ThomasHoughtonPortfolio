95e1d71ed3c1cf9e0bf6af518f79bd2278634889
tomhoughton <tomhoughton888@gmail.com>
2022-09-22 13:59:08 +0100

    Now Gets data from server

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React, { useEffect, useState, setState } from 'react'; 
 import './App.css';
 
+// TODO: Make sure we are getting the necesarry data from the server:
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	const [ projects, setProjects ] = useState({});
+
+	useEffect(() => {
+		fetch("/api/projects")
+			.then(res => res.json())
+			.then(data => console.log(data))
+	});
+	
+	return (
+		<div className="App">
+			
+		</div>
+	);
 }
 
 export default App;
