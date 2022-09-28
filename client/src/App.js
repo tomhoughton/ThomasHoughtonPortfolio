0a6e6f6581375d27ee2ce731441adaac44dbbbf7
tomhoughton <tomhoughton888@gmail.com>
2022-09-28 12:26:14 +0100

    React App

--- client/src/App.js
+++ client/src/App.js
@@ -1,74 +1,25 @@
-import React, { useEffect, useState, setState } from 'react';
-import { Navigation, Pagination } from 'swiper';  
-import { Swiper, SwiperSlide } from 'swiper/react';
-import CarouselCard from './components/CarouselCard';
-import { ChakraProvider, Container, Grid, GridItem } from '@chakra-ui/react';
-import { ReactComponent as Tile } from './drawing.svg';
-import { Row, Col } from 'react-grid-system';
-import anime from 'animejs/lib/anime.es';
-import Welcome from './components/Welcome';
-
-// Styles:
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+import logo from './logo.svg';
 import './App.css';
-import ProjectDetails from './components/ProjectDetails';
-
-// TODO: Make sure we are getting the necesarry data from the server:
 
 function App() {
-
-	const [ projects, setProjects ] = useState([]);
-	const [ selectedProjectIndex, selectProjectIndex] = useState(0);
-
-	useEffect(() => {
-		fetch("/api/projects")
-			.then(res => res.json())
-			.then(data => setProjects(data.projectsReturn))
-	}, []);
-	
-	const carouselClick = (i) => {
-		selectProjectIndex(i);
-	}
-
-	const projectRender = () => {
-		
-	}
-
-	anime({
-		targets: '.drawing',
-		strokeDashoffset: [anime.setDashoffset, 0],
-		easing: 'cubicBezier(.5, .05, .1, .3)',
-		duration: 1000,
-		delay: function(el, i) { return i * 250 },    
-	});
-
-	return (
-		<ChakraProvider>
-			<Welcome />     
-		 
-			<Swiper
-				modules={[Navigation, Pagination]}
-				spaceBetween={50}
-				slidesPerView={3}
-				onSlideChange={() => console.log('slide change')}
-				onSwiper={(swiper) => console.log(swiper)}
-			>	
-				{
-					projects.map((x, i) => {
-						return (
-							<SwiperSlide>
-								<CarouselCard index={i} selectedProject={selectedProjectIndex} title={x.name} onChange={carouselClick}/>
-							</SwiperSlide>
-						)
-					})
-				}
-			</Swiper>
-			
-			<ProjectDetails title={selectedProjectIndex} project={projects[selectedProjectIndex]} />
-		</ChakraProvider>
-	);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
