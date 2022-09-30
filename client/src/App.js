1de67a21d43a94870ed6a2b027482450b3d7de56
Thomas Houghton <tomhoughton888@gmail.com>
2022-09-30 18:16:38 +0100

    Better Controls

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,26 @@ import { FullPage, Slide } from 'react-full-page';
 function App() {
   return (
     <FullPage>
+      
+      <div className=' flex flex-col fixed right-0 h-screen'>
+        <div className='m-auto'>
+          <div className='flex flex-row m-4'>
+            <p className='align-text-top invisible md:visible'>Hello</p>
+            <div className='w-2 h-2 md:w-4 md:h-4 bg-green-400 rounded-full ml-2 m-auto'></div>
+          </div>
+          <div className='flex flex-row m-4'>
+            <p className='align-text-center invisible md:visible'>Hello</p>
+            <div className='w-2 h-2 md:w-4 md:h-4 bg-green-400 rounded-full ml-2 m-auto'></div>
+          </div>
+          <div className='flex flex-row m-4'>
+            <p className='align-text-top invisible md:visible'>Hello</p>
+            <div className='w-2 h-2 md:w-4 md:h-4 bg-green-400 rounded-full ml-2 m-auto'></div>
+          </div>
+        </div>
+      </div>
+
+
+
       <Slide className="bg-slate-500">
         <h1>Slide01</h1>
       </Slide>
